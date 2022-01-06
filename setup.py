5b8f4fefb12c4919aa2ad72162a5326214653c80
Marc Wimmer <marc@itewimmer.de>
2022-01-06 01:20:17 +0100

    main: [main]

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ class UploadCommand(Command):
         self.clear_builds()
 
         self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        os.system('{0} setup.py sdist'.format(sys.executable))
 
         self.status('Uploading the package to PyPI via Twine…')
         os.system('twine upload dist/*')
