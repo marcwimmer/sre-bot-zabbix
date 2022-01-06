c1e9979dc79f68d8b74434e400a5f378f53d839a
Marc Wimmer <marc@itewimmer.de>
2022-01-06 01:05:03 +0100

    main: [main]

--- setup.py
+++ setup.py
@@ -8,27 +8,20 @@ import io
 import os
 import sys
 from glob import glob
-import importlib
-import tempfile
-import shutil
-import distutils
-import subprocess
 from shutil import rmtree
 from pathlib import Path
 
-from setuptools import find_packages, setup, Command
+from setuptools.config import read_configuration
+from setuptools import setup, Command
 from setuptools.command.install import install
 from subprocess import check_call, check_output
+
+setup_cfg = read_configuration("setup.cfg")
+metadata = setup_cfg['metadata']
 
 # Package meta-data.
 NAME = 'sre-bot-zabbix'
 BOTS_PATH = "sre-bots/" + NAME # e.g. /var/lib/sre-bot/sre-bots/sre-bot-zabbix
-DESCRIPTION = 'Data collector / executor - Site Reliability Framework'
-URL = 'https://github.com/marcwimmer/sre-bot-zabbix'
-EMAIL = 'marc@itewimmer.de'
-AUTHOR = 'Marc-Christian Wimmer'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -40,29 +33,22 @@ EXTRAS = {
     # 'fancy feature': ['django'],
 }
 
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         long_description = '\n' + f.read()
 except FileNotFoundError:
-    long_description = DESCRIPTION
+    long_description = metadata['DESCRIPTION']
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-if not VERSION:
+if not metadata['version']:
     project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
     with open(os.path.join(here, project_slug, '__version__.py')) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about['__version__'] = metadata['version']
 
 
 class UploadCommand(Command):
@@ -82,12 +68,16 @@ class UploadCommand(Command):
     def finalize_options(self):
         pass
 
+    def clear_builds(self):
+        for path in ['dist', 'build', NAME.replace("-", "_") + ".egg-info"]:
+            try:
+                self.status(f'Removing previous builds from {path}')
+                rmtree(os.path.join(here, path))
+            except OSError:
+                pass
+
     def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
+        self.clear_builds()
 
         self.status('Building Source and Wheel (universal) distribution…')
         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
@@ -98,6 +88,8 @@ class UploadCommand(Command):
         self.status('Pushing git tags…')
         os.system('git tag v{0}'.format(about['__version__']))
         os.system('git push --tags')
+
+        self.clear_builds()
 
         sys.exit()
 
@@ -115,39 +107,20 @@ class InstallCommand(install):
             path = Path("/usr/local") / BOTS_PATH
         if path.exists():
             check_call(["sre", "add-bot-path", path])
-class UninstallCommand(install):
-    def run(self):
-        install.run(self)
 
 setup(
-    name=NAME,
     version=about['__version__'],
-    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    # packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    #py_modules=['srebot'],
-
-    entry_points={
-    },
-    package_data={
-    },
     data_files=[
         (BOTS_PATH, glob(NAME.replace("-", "_") + "/*")),
     ],
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='MIT',
     # $ setup.py publish support.
     cmdclass={
         'upload': UploadCommand,
         'install': InstallCommand,
-        'uninstall': UninstallCommand,
     },
 )
